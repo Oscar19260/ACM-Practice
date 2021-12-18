c52783ed02943a66265ae962b7829dafbd68b70f
Oz26 <oscar19260@gmail.com>
2021-12-17 19:38:12 -0600

    Taxi

---
+++ Codeforces/taxi.py
@@ -0,0 +1,38 @@
+def ntaxis(n, s):
+  taxis = 0
+  agroup = 0
+  i, j = 0, n-1
+
+  if sum(s) <= 4:
+    taxis += 1
+    return taxis
+
+  while i != j+1:
+    if agroup == 0:
+      agroup += s[j]
+    if agroup == 4:
+      taxis += 1
+      j -= 1
+      agroup = 0
+    elif i == j:
+      taxis += 1
+      break
+    else:
+      agroup += s[i]
+      if agroup == 4:
+        taxis += 1
+        j -= 1
+        i += 1
+        agroup = 0
+      elif agroup > 4:
+        taxis += 1
+        j -= 1
+        agroup = 0
+      else:
+        i += 1
+  return taxis
+
+n = int(input())
+s = list(map(int, input().split()))
+s.sort()
+print(ntaxis(n, s))
